9f5fa9a2004c4094f164832d624fa83c3aacfdee
sergioandres16 <sergio.sotomayor@pucp.edu.pe>
2025-02-03 03:02:42 -0500

    2/3 pt.3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DashboardComponent } from './components/modulos/dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent), canActivate: [AuthGuard] },
+  { path: 'dashboard', loadComponent: () => import('./components/modulos/dashboard/dashboard.component').then(m => m.DashboardComponent), canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'login' }
 ];
